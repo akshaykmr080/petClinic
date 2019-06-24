0ce57648582b1596690bca043933f817d561f7d0
Akshay Kumar Shreekanth <shreekan@usc.edu>
2019-06-23 20:49:37 -0700

    Added Speciality to vets

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
@@ -0,0 +1,17 @@
+package com.example.petclinic.model;
+
+/**
+ * Created by akshaykumar on 6/23/19.
+ */
+public class Speciality extends BaseEntity {
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
