028f3b46f64fac2557d3aa99903638dd170b0dcd
Akshay Kumar Shreekanth <shreekan@usc.edu>
2019-06-24 23:45:58 -0700

    Added SpecialitiesService

---
+++ pet-clinic-data/src/main/java/com/example/petclinic/services/SpecialitiesService.java
@@ -0,0 +1,9 @@
+package com.example.petclinic.services;
+
+import com.example.petclinic.model.Speciality;
+
+/**
+ * Created by akshaykumar on 6/24/19.
+ */
+public interface SpecialitiesService extends CRUDService<Speciality, Long> {
+}
