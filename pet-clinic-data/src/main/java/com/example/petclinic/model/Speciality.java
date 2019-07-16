ebc5f76da830ea0ce5e27d6d44e41973612250a2
Akshay Kumar Shreekanth <shreekan@usc.edu>
2019-07-15 21:39:55 -0700

    Updated vet with entity mappings

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Speciality.java
@@ -1,10 +1,18 @@
 package com.example.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * Created by akshaykumar on 6/23/19.
  */
+
+@Entity
+@Table(name = "speciality")
 public class Speciality extends BaseEntity {
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
