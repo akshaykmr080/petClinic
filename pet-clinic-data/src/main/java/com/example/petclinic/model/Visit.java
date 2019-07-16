ae759fe92d2825e9acb90e0a3cfe180656c1e6f7
Akshay Kumar Shreekanth <shreekan@usc.edu>
2019-07-15 21:51:41 -0700

    Added Visit entity

--- pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
+++ pet-clinic-data/src/main/java/com/example/petclinic/model/Visit.java
@@ -1,16 +1,24 @@
 package com.example.petclinic.model;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
 /**
  * Created by akshaykumar on 6/23/19.
  */
+
+@Entity
+@Table(name = "Visit")
 public class Visit extends BaseEntity {
 
+    @Column(name = "date")
     private LocalDate date;
 
+    @Column(name = "description")
     private String description;
 
+    @ManyToOne
+    @JoinColumn(name = "pet_id")
     private Pet pet;
 
     public LocalDate getDate() {
